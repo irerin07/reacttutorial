98fe786f7e86966a85c322f8a31321bcbe7cdb30
irerin07 <counterchord89@gmail.com>
2019-09-30 00:39:29 +0900

    JSX 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react"; // React를 불러와서 사용할 수 있게 해준다
+import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1>리액트 안녕!</h1>
-      <h2>잘 작동하니?</h2>
-    </>
-  );
+  const name = "리액트";
+
+  return <div className="react">{name}</div>;
 }
 
 export default App;
