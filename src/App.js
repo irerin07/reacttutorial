22996041d4f62be9ef1be69adc2e5b75e8dd8c98
irerin07 <counterchord89@gmail.com>
2019-09-29 21:59:00 +0900

    JSX 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; // React를 불러와서 사용할 수 있게 해준다
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>리액트 안녕!</h1>
+      <h2>잘 작동하니?</h2>
+    </>
   );
 }
 
