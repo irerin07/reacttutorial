8478cc7b02ee2eb9663021d30cf99e2d2f426346
irerin07 <counterchord89@gmail.com>
2019-10-31 01:51:58 +0900

    hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, { Component } from "react"; // React를 불러와서 사용할 수 있게 해준다
+import React, { useState } from "react"; // React를 불러와서 사용할 수 있게 해준다
 // import MyComponent from "./MyComponent";
 // import Say from "./Say";
 // import EventPractice from "./EventPractice";
 // import ValidationSample from "./ValidationSample";
 // import ScrollBox from "./ScrollBox";
-import IterationSample from "./IterationSample";
+// import IterationSample from "./IterationSample";
+// import CounterHooks from "./Counter_hooks";
+import Info from "./Info.js";
 
-class App extends Component {
-  render() {
-    return <IterationSample />;
-  }
-}
+// class App extends Component {
+//   render() {
+//     return <CounterHooks />;
+//   }
+// }
+
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "hide" : "show"}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
+};
 export default App;
